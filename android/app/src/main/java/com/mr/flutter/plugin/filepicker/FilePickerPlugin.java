f39143f0468c1db746b72fecce9b7c2c8be5e3b1
Wintermute <wintermute@biohacker.dev>
2026-03-04 17:45:08 -0500

    Add file_picker stub + aggressive cache cleanup in CI

---
+++ android/app/src/main/java/com/mr/flutter/plugin/filepicker/FilePickerPlugin.java
@@ -0,0 +1,9 @@
+package com.mr.flutter.plugin.filepicker;
+
+/**
+ * Stub implementation - file_picker not used in this app.
+ * Using native Android file picker via method channels instead.
+ */
+public class FilePickerPlugin {
+    // Intentionally empty
+}
