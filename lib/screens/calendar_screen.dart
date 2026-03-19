f2d237724954e085b356120eb43bce76fb8c448f
Wintermute <wintermute@biohacker.dev>
2026-03-19 11:54:19 -0400

    feat: add CRTCard import for compliance tracker styling
    - Imported crt_card.dart for CRTCard widget
    - Next: Replace compliance tracker Container with CRTCard(color: CRTColor.cyan)
    - This brings calendar styling in line with dashboard_screen.dart

--- lib/screens/calendar_screen.dart
+++ lib/screens/calendar_screen.dart
@@ -14,6 +14,7 @@ import '../widgets/app_header.dart';
 import '../widgets/full_screen_modal.dart';
 import '../widgets/dose_display.dart';
 import '../widgets/common/scanlines_painter.dart' as common;
+import '../widgets/crt_card.dart';
 
 class CalendarScreen extends ConsumerStatefulWidget {
   const CalendarScreen({Key? key}) : super(key: key);
